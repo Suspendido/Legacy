bdb6789cb3f22f856553bfdfa79792af135d1421
Comunidad <correotrabajoiWnts@hotmail.com>
2026-04-17 23:45:06 -0600

    fix: renamed command class name to not confuse it with main command

--- src/main/java/me/comunidad/dev/legacy/module/commands/type/CombatEditorCommand.java
+++ src/main/java/me/comunidad/dev/legacy/module/commands/type/CombatEditorCommand.java
@@ -7,6 +7,7 @@ import me.comunidad.dev.legacy.module.commands.CommandManager;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
 import java.util.List;
 
 /*
@@ -14,10 +15,10 @@ import java.util.List;
  * Use or redistribution of this source file is only permitted
  * if explicit permission is given by the author.
  */
-public class LegacyEditorCommand extends Command {
+public class CombatEditorCommand extends Command {
 
-    public LegacyEditorCommand(CommandManager manager) {
-        super(manager, "menu");
+    public CombatEditorCommand(CommandManager manager) {
+        super(manager, "combat");
         this.setPermissible("legacy.menu");
     }
 
@@ -28,16 +29,27 @@ public class LegacyEditorCommand extends Command {
 
     @Override
     public List<String> usage() {
-        return List.of();
+        return List.of("&cUsage: &c/combat menu - Opens the ingame editor");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sendMessage(sender, Config.PLAYER_ONLY);
-            return;
-        }
+        switch (args[0].toLowerCase()) {
+            case "menu" -> {
+                if (!(sender instanceof Player player)) {
+                    sendMessage(sender, Config.PLAYER_ONLY);
+                    return;
+                }
+                new MainMenu(getInstance().getMenuManager(), player).open();
+            }
 
-        new MainMenu(getInstance().getMenuManager(), player).open();
+            default -> sendUsage(sender);
+        }
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, String[] args) {
+        if (args.length == 0) return Collections.singletonList("menu");
+        return Collections.emptyList();
     }
 }
